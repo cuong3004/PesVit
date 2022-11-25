a2e277b7e400b99d715615abba43c497dd2aa69f
cuong3004 <65102570+cuong3004@users.noreply.github.com>
2022-11-25 20:31:14 +0700

    update

--- ghostpes/fine.py
+++ ghostpes/fine.py
@@ -63,7 +63,7 @@ class PesDataModule(pl.LightningDataModule):
         if stage == 'fit' or stage is None:
             self.data_train = ImageFolder("/content/dataset/train", transform=self.transform_train)
             self.data_val = ImageFolder("/content/dataset/valid", transform=self.transform_valid)
-            # self.data_val = ImageFolder("/content/dataset/test", transform=self.transform_test)
+            self.data_test = ImageFolder("/content/dataset/test", transform=self.transform_test)
 
         # self.train_dataloader
         
@@ -75,7 +75,7 @@ class PesDataModule(pl.LightningDataModule):
         return DataLoader(self.data_val, batch_size=self.batch_size)
 
     def test_dataloader(self):
-        return DataLoader(self.data_val, batch_size=self.batch_size)
+        return DataLoader(self.data_test, batch_size=self.batch_size)
 
 from torchmetrics.functional import accuracy, precision, recall, f1_score
 average = 'macro'
@@ -102,6 +102,9 @@ class LitModel(pl.LightningModule):
         self.acc = accuracy
         self.pre = precision
         self.rec = recall
+        
+        self.all_preds = []
+        self.all_labels = []
     
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -119,21 +122,30 @@ class LitModel(pl.LightningModule):
         
         return loss
     
-    # def validation_step(self, batch, batch_idx):
-    #     x, y = batch
-    #     logits = self.model(x)
-    #     loss = F.cross_entropy(logits, y)
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.model(x)
+        
+        
+        pred = logits.argmax(dim=1)
+        
+        self.all_preds.append(pred.to('cpu'))
+        self.all_labels.append(y.to('cpu'))
+            
+        
+        
+        # # loss = F.cross_entropy(logits, y)
 
-    #     # validation metrics
-    #     # preds = torch.argmax(logits, dim=1)
-    #     acc = self.acc(logits, y, num_classes=2)
-    #     # pre = self.pre(logits, y, average=average, num_classes=2)
-    #     # rec = self.rec(logits, y, average=average, num_classes=2)
-    #     self.log('val_loss', loss, on_step=False, on_epoch=True)
-    #     # self.log('val_acc', acc, on_step=False, on_epoch=True)
-    #     # self.log('val_pre', pre, on_step=False, on_epoch=True)
-    #     # self.log('val_rec', rec, on_step=False, on_epoch=True)
-    #     return loss
+        # # validation metrics
+        # # preds = torch.argmax(logits, dim=1)
+        # acc = self.acc(logits, y, num_classes=2)
+        # # pre = self.pre(logits, y, average=average, num_classes=2)
+        # # rec = self.rec(logits, y, average=average, num_classes=2)
+        # self.log('val_loss', loss, on_step=False, on_epoch=True)
+        # # self.log('val_acc', acc, on_step=False, on_epoch=True)
+        # # self.log('val_pre', pre, on_step=False, on_epoch=True)
+        # # self.log('val_rec', rec, on_step=False, on_epoch=True)
+        # return loss
     
     def on_train_start(self):
         self.log('val_acc', 0)
@@ -141,27 +153,10 @@ class LitModel(pl.LightningModule):
         self.log('val_rec', 0)
         self.log('val_f1', 0)
     
-    def on_train_epoch_end(self):
+    def on_valid_end(self):
         
-        self.eval()
-        self.model.to(self.device)
-        
-        all_preds = []
-        all_labels = []
-        
-        for batch in dm.val_dataloader():
-            with torch.no_grad():
-                x, y= batch
-                x = x.to(self.device)
-                # y = y.to(self.device)
-                logits = self.model(x)
-
-            pred = logits.to("cpu").argmax(dim=1)
-            all_preds.append(pred)
-            all_labels.append(y)
-            
-        all_preds = torch.cat(all_preds,dim=0)
-        all_labels = torch.cat(all_labels,dim=0)
+        all_preds = torch.cat(self.all_preds,dim=0)
+        all_labels = torch.cat(self.all_labels,dim=0)
         
         acc = accuracy(all_preds, all_labels)
         pre = precision(all_preds, all_labels, average=average, num_classes=2)
@@ -173,25 +168,89 @@ class LitModel(pl.LightningModule):
         self.log('val_rec', rec)
         self.log('val_f1', f1)
         
+        self.all_preds = []
+        self.all_preds = []
+    
+    # def on_train_epoch_end(self):
+        
+    #     self.eval()
+    #     self.model.to(self.device)
+        
+    #     all_preds = []
+    #     all_labels = []
+        
+    #     for batch in dm.val_dataloader():
+    #         with torch.no_grad():
+    #             x, y= batch
+    #             x = x.to(self.device)
+    #             # y = y.to(self.device)
+    #             logits = self.model(x)
+
+    #         pred = logits.to("cpu").argmax(dim=1)
+    #         all_preds.append(pred)
+    #         all_labels.append(y)
+            
+    #     all_preds = torch.cat(all_preds,dim=0)
+    #     all_labels = torch.cat(all_labels,dim=0)
+        
+    #     acc = accuracy(all_preds, all_labels)
+    #     pre = precision(all_preds, all_labels, average=average, num_classes=2)
+    #     rec = recall(all_preds, all_labels, average=average, num_classes=2)
+    #     f1 = f1_score(all_preds, all_labels, average=average, num_classes=2)
+        
+    #     self.log('val_acc', acc)
+    #     self.log('val_pre', pre)
+    #     self.log('val_rec', rec)
+    #     self.log('val_f1', f1)
+        
         # print("accuracy")
         
         # for i 
+    
+    # def on_train_start(self):
+    #     self.
         
-    # def test_step(self, batch, batch_idx):
-    #     x, y = batch
-    #     logits = self.model(x)
-    #     loss = F.cross_entropy(logits, y)
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.model(x)
+        
+        
+        pred = logits.argmax(dim=1)
+        
+        self.all_preds.append(pred.to('cpu'))
+        self.all_labels.append(y.to('cpu'))
+    
+    def on_test_end(self):
+        
+        all_preds = torch.cat(self.all_preds,dim=0)
+        all_labels = torch.cat(self.all_labels,dim=0)
+        
+        acc = accuracy(all_preds, all_labels)
+        pre = precision(all_preds, all_labels, average=average, num_classes=2)
+        rec = recall(all_preds, all_labels, average=average, num_classes=2)
+        f1 = f1_score(all_preds, all_labels, average=average, num_classes=2)
+        
+        self.log('test_acc', acc)
+        self.log('test_pre', pre)
+        self.log('test_rec', rec)
+        self.log('test_f1', f1)
+        
+        self.all_preds = []
+        self.all_preds = []
+        # x, y = batch
+        # logits = self.model(x)
+        # loss = F.cross_entropy(logits, y)
 
-    #     # validation metrics
-    #     # preds = torch.argmax(logits, dim=1)
-    #     acc = self.accuracy(preds, y)
-    #     pre = self.pre(preds, y)
-    #     rec = self.rec(preds, y)
-    #     self.log('test_loss', loss, on_step=False, on_epoch=True)
-    #     self.log('test_acc', acc, on_step=False, on_epoch=True)
-    #     self.log('test_pre', pre, on_step=False, on_epoch=True)
-    #     self.log('test_rec', rec, on_step=False, on_epoch=True)
-    #     return loss
+        # # validation metrics
+        # # preds = torch.argmax(logits, dim=1)
+        # acc = self.accuracy(preds, y)
+        # pre = self.pre(preds, y)
+        # rec = self.rec(preds, y)
+        # self.log('test_loss', loss, on_step=False, on_epoch=True)
+        # self.log('test_acc', acc, on_step=False, on_epoch=True)
+        # self.log('test_pre', pre, on_step=False, on_epoch=True)
+        # self.log('test_rec', rec, on_step=False, on_epoch=True)
+        # return loss
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
@@ -224,4 +283,8 @@ trainer = pl.Trainer(max_epochs=100,
 
 # Train the model ⚡🚅⚡
 trainer.fit(model_lit, dm)
-# trainer.test(model_lit, dm, ckpt_path='best')
+
+model_lit.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
+trainer.test(model_lit, dm)
+# print(trainer.checkpoint_callback.best_model_path)
+
